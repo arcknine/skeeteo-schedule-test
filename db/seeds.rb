4628c89582851bb7d25f02dabb7b44c9123683a0
Keenan Iban <keenan.iban@gmail.com>
2019-12-11 22:18:44 +0800

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Admins
+Admin.delete_all
+Admin.create(:email => "admin@email.com", :password => "passw0rd", :password_confirmation => "passw0rd")
